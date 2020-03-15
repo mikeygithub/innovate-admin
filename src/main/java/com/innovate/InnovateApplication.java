9b5046a2f2c2e89ac4130366dddfd3f4d3d0b2ad
mikeygit <biaogejiushibiao@outlook.com>
2020-03-15 17:29:04 +0800

    add jasypt

--- src/main/java/com/innovate/InnovateApplication.java
+++ src/main/java/com/innovate/InnovateApplication.java
@@ -1,6 +1,7 @@
 package com.innovate;
 
 import com.innovate.datasources.DynamicDataSourceConfig;
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -8,7 +9,6 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Import;
-
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @Import({DynamicDataSourceConfig.class})
