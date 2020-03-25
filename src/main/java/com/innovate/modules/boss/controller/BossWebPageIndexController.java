09c7afbed4b274c1f9559a19b2798c123156994e
mikeygit <biaogejiushibiao@outlook.com>
2020-03-26 02:10:46 +0800

    fix bug

--- src/main/java/com/innovate/modules/boss/controller/BossWebPageIndexController.java
+++ src/main/java/com/innovate/modules/boss/controller/BossWebPageIndexController.java
@@ -30,7 +30,7 @@ import java.util.Map;
  * @date 2019/10/15
  */
 @RestController
-@RequestMapping("/webpage")
+@RequestMapping("webpage")
 public class BossWebPageIndexController {
 
     @Autowired
@@ -38,15 +38,6 @@ public class BossWebPageIndexController {
 
     @Autowired
     private SysUserService sysUserService;
-
-    @Autowired
-    private UserTeacherInfoService userTeacherInfoService;
-
-    @Autowired
-    private UserPerInfoService userPerInfoService;
-
-    @Autowired
-    private SysUserRoleService sysUserRoleService;
 
     private String VerificationCode;//短信验证码
 
@@ -109,7 +100,6 @@ public class BossWebPageIndexController {
                        @RequestParam( name = "pwd", required = false, defaultValue = "123456") String pwd,
                        @RequestParam( name = "ex", required = false) String ex,
                        @RequestParam( name = "type", required = false, defaultValue = "1")String type){
-        // new Sha256Hash(user.getPassword(), salt).toHex()
         if(!"".equals(tk) && "1".equals(type)){// 单个更新密码
             SysUserEntity sysUserEntity = sysUserService.queryByUserName(tk);
             if(sysUserEntity != null){
@@ -155,7 +145,6 @@ public class BossWebPageIndexController {
             return R.ok().put("result", res);
         }
     }
-
     /**
      * 账号/密码找回
      */
@@ -179,73 +168,6 @@ public class BossWebPageIndexController {
         } else {
             return R.ok().put("come","0");
         }
-
         return R.ok().put("come","1");
     }
-
-//    @RequestMapping("per")
-//    public R templatePer(){
-//        //List<SysUserEntity> list = sysUserService.selectList(new EntityWrapper<SysUserEntity>());
-//        List<UserPersonInfoEntity> list = userPerInfoService.selectList(new EntityWrapper<UserPersonInfoEntity>());
-//        List<HashMap<Object,Object>> result = new ArrayList<HashMap<Object,Object>>();
-//        if(list != null && list.size() > 0){
-//            for (int i = 0; i < list.size(); i++) {
-//                HashMap<Object, Object> map = new HashMap<>();
-//                UserPersonInfoEntity entity = list.get(i);
-//                map.put("学生",entity.getUserId());
-//                List<Long> longs = sysUserRoleService.queryRoleIdList(entity.getUserId());
-//                if(longs != null){
-//                    boolean flag = false;
-//                    for (int j = 0; j < longs.size(); j++){
-//                        if(longs.get(j).longValue() == 11L){
-//                            flag = true;
-//                            map.put("角色","有学生角色");
-//                        }
-//                    }
-//                    if(!flag){
-//                        longs.add(11L);
-//                        sysUserRoleService.saveOrUpdate(entity.getUserId(),longs);
-//                        map.put("是否新增成功", "是");
-//                    }else{
-//                        map.put("是否新增成功", "否");
-//                    }
-//                }
-//                result.add(map);
-//            }
-//        }
-//        return R.ok().put("data", result);
-//    }
-//
-//    @RequestMapping("tea")
-//    public R templateTea(){
-//        List<UserTeacherInfoEntity> list = userTeacherInfoService.selectList(new EntityWrapper<UserTeacherInfoEntity>());
-//        List<HashMap<Object,Object>> result = new ArrayList<HashMap<Object,Object>>();
-//        if(list != null && list.size() > 0){
-//            for (int i = 0; i < list.size(); i++) {
-//                HashMap<Object, Object> map = new HashMap<>();
-//                UserTeacherInfoEntity entity = list.get(i);
-//                map.put("教师",entity.getUserId());
-//                List<Long> longs = sysUserRoleService.queryRoleIdList(entity.getUserId());
-//                if(longs != null){
-//                    boolean flag = false;
-//                    for (int j = 0; j < longs.size(); j++){
-//                        if(longs.get(j).longValue() == 12L){
-//                            flag = true;
-//                            map.put("角色","有教师角色");
-//                        }
-//                    }
-//                    if(!flag){
-//                        longs.add(12L);
-//                        sysUserRoleService.saveOrUpdate(entity.getUserId(),longs);
-//                        map.put("是否新增成功", "是");
-//                    }else{
-//                        map.put("是否新增成功", "否");
-//                    }
-//                }
-//                result.add(map);
-//            }
-//        }
-//        return R.ok().put("data", result);
-//    }
-
 }
